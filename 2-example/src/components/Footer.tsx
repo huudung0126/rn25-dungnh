b0fda2d97cff0611743ae3cb46506eabeade8245
dungnh <ndung1829@gmail.com>
2022-10-10 19:47:08 +0700

    dungnh

---
+++ 2-example/src/components/Footer.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+type Props = {};
+
+type State = {};
+
+class Footer extends Component<Props, State> {
+  state = {};
+
+  render() {
+    return (
+      <>
+        <div className="footer">
+        <p className="text-center p-1 m-0">CopyRight</p>
+        </div>
+        <div className="container-fluid"></div>
+      </>
+    );
+  }
+}
+
+export default Footer;
---
+++ 2-example/src/components/Header.tsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export default class Header extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <>
+      <div className = "header">
+      <h1 className="header_title pt-2 pb-2 m-0 ">The Walk</h1>
+      </div>
+      <div className="wrapper">
+      <div className="container-fluid">
+        
+        
+      </div>
+      </div>
+      </>
+      
+    )
+  }
+}
